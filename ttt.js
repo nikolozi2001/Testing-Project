443d718fef16eec36607c5bd52615b5d36f85e75
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-10-27 09:23:21 +0400

    feat: Implement unique combination validation for country and currency in data processing

--- ttt.js
+++ ttt.js
@@ -1,1 +1,14 @@
-show = row["pageTestDataGrid3"].value == 5 ? true : false;
+let uniqueCombinations = new Set();
+
+for (let i = 0; i < data.page2msheneblobaArarezidenti.length; i++) {
+  const country = data.page2msheneblobaArarezidenti[i]["page66"].value;
+  const currency = data.page2msheneblobaArarezidenti[i]["page67"].value;
+  const combination = `${country}_${currency}`;
+
+  if (uniqueCombinations.has(combination)) {
+    valid = "რომელიმე მომსახურება კონკრეტული ქვეყნიდან და კონკრეტულ ვალუტაში, მხოლოდ ერთხელ უნდა შეივსოს მთლიანი თანხის მითითებით !";
+    break;
+  } else {
+    uniqueCombinations.add(combination);
+  }
+}
