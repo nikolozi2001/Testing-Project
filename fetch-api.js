617f609b854ee76310d108139399d39f8585a55e
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-04-02 11:00:27 +0400

    feat: Update validation messages in sv5.js for clarity and consistency; add fetch-api.js for enterprise survey data handling

---
+++ fetch-api.js
@@ -0,0 +1,35 @@
+const enterpriseSurveyId = window.location.href.split("/").pop();
+const url = `/api/enterprise/get-survey-page-show-param/${enterpriseSurveyId}`;
+const key = `surveyPageShow_${enterpriseSurveyId}`;
+
+
+if (!window.enterprisePageShowMap) {
+    window.enterprisePageShowMap = new Map();
+}
+
+
+if (!window.enterprisePageShowMap.has(key)) {
+
+    $.ajax({
+        url: "/api/enterprise/get-survey-page-show-param/" + enterpriseSurveyId,
+        type: 'GET',
+        async: false,
+        headers: {"Authorization": localStorage.getItem('accessToken')},
+        error: function (err) {
+           throw new Error("Failed to fetch survey data");
+        },
+        success: function (response) {
+            window.enterprisePageShowMap.set(key, response);
+        }
+    });
+}
+
+
+let value = window.enterprisePageShowMap.get(key).toString();
+
+
+if (value === "2") {
+  show = false;
+} else {
+  show = true;
+}
