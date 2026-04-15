64ad6b531d72be4da38fce33bc63781d69aee707
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-04-15 12:20:41 +0400

    refactor: Simplify data fetching logic in fetch3.js and enhance readability

--- modules/fetch3.js
+++ modules/fetch3.js
@@ -1,44 +1,30 @@
-var url = window.location.href;
-var idMatch = url.match(/\/(\d+)(?:\?|$)/);
-var id = idMatch ? idMatch[1] : url.split("/").pop();
+var id = window.location.href.split("/").pop();
+var fieldKey = "compensationOfEmployedPersonnelFemale";
 
 window.workSurvData = window.workSurvData || {};
 
-if (id === "451846") {
-    window.workSurvData[id] = [
-        { compensationOfEmployedPersonnelFemale: 111 },
-        { compensationOfEmployedPersonnelFemale: 222 },
-        { compensationOfEmployedPersonnelFemale: 333 }
-    ];
-} 
-else if (id && !window.workSurvData[id]) {
-    $.ajax({
-        url: "/api/enterprise/prev-work-survey-data?enterpriseSurveyId=" + id,
-        method: "GET",
-        async: false,
-        headers: {
-            Accept: "application/json",
-            Authorization: localStorage.getItem("accessToken"),
-        },
-        success: function (res) { 
-            window.workSurvData[id] = res; 
-        },
-        error: function() { 
-            window.workSurvData[id] = []; 
-        }
-    });
+if (id) {
+  $.ajax({
+    url: "/api/enterprise/prev-work-survey-data?enterpriseSurveyId=" + id,
+    method: "GET",
+    async: false,
+    headers: {
+      Accept: "application/json",
+      Authorization: localStorage.getItem("accessToken"),
+    },
+    success: function (res) {
+      window.workSurvData[id] = res;
+      console.log("workSurvData API response [id=" + id + "]:", res);
+    },
+    error: function () {
+      window.workSurvData[id] = [];
+    },
+  });
 }
 
 var rowIndex = instance.rowIndex;
+var surveyRow = id && window.workSurvData[id] && window.workSurvData[id][rowIndex];
 
-console.log("FINAL Row Index:", rowIndex);
-
-if (
-    typeof rowIndex !== "undefined" &&
-    window.workSurvData[id] &&
-    window.workSurvData[id][rowIndex]
-) {
-    value = window.workSurvData[id][rowIndex].compensationOfEmployedPersonnelFemale;
-} else {
-    value = 0;
-}
+value = (typeof rowIndex !== "undefined" && surveyRow && surveyRow[fieldKey] != null)
+  ? surveyRow[fieldKey]
+  : 0;
