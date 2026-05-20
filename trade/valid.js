cc31eea53997c963192bc141d5b467dce83688b9
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-05-20 10:37:09 +0400

    Add validation for unique country-currency combinations in trade data
    - Introduced a new script to ensure that each service from a specific country and currency is only entered once with the total amount specified.
    - Utilized a Set to track unique combinations and provide an error message if duplicates are found.

---
+++ trade/valid.js
@@ -0,0 +1,15 @@
+let uniqueCombinations = new Set();
+
+for (let i = 0; i < data.page2SaqonlisRemonti.length; i++) {
+  const country = data.page2SaqonlisRemonti[i]["page45"].value;
+  const currency = data.page2SaqonlisRemonti[i]["page7"].value;
+  const combination = `${country}_${currency}`;
+
+  if (uniqueCombinations.has(combination)) {
+    valid =
+      "რომელიმე მომსახურება კონკრეტული ქვეყნიდან და კონკრეტულ ვალუტაში, მხოლოდ ერთხელ უნდა შეივსოს მთლიანი თანხის მითითებით !";
+    break;
+  } else {
+    uniqueCombinations.add(combination);
+  }
+}
