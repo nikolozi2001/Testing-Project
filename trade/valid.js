d97b9206ae2cf228b85eaac5d21cbef4541b5e93
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-05-20 11:26:53 +0400

    Add logic to check for duplicate country-currency combinations
    - Implemented a new check in valid.js to identify duplicate combinations of country and currency from the data.page2SaqonlisRemonti array.
    - Introduced a new valid.jsonl file containing a logic definition for the duplicate check, including a disable action for the relevant property.

--- trade/valid.js
+++ trade/valid.js
@@ -13,3 +13,21 @@ for (let i = 0; i < data.page2SaqonlisRemonti.length; i++) {
     uniqueCombinations.add(combination);
   }
 }
+
+
+let uniqueCombinations = new Set();
+result = false;
+
+for (let i = 0; i < data.page2SaqonlisRemonti.length; i++) {
+  const country = data.page2SaqonlisRemonti[i].page45;
+  const currency = data.page2SaqonlisRemonti[i].page7;
+  const combination = `${country}_${currency}`;
+
+  if (uniqueCombinations.has(combination)) {
+    result = true;
+    break;
+  } else {
+    uniqueCombinations.add(combination);
+  }
+}
+
