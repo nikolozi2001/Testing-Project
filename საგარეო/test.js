a42037e04873f0f3f9dab59171da73fe678c829c
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-07-07 12:37:25 +0400

    trying to fix instantly validation

--- საგარეო/test.js
+++ საგარეო/test.js
@@ -1,34 +1,72 @@
-function isEmpty(str) {
-  return !str;
+let sum = 0;
+const e1S2 = row["pageVPanelTable12IiiIv"] || 0;
+const pStart = row["pageVPanelTable12IiIii"] || 0;
+const p520 = row["pageVPanelTable12Iii"] || 0;
+const p522 = row["pageVPanelTable12Number3"] || 0;
+const p530 = row["pageVPanelTable10Iii"] || 0;
+const p531 = row["pageVPanelTable10Iii2"] || 0;
+const p532 = row["pageVPanelTable10Number3"] || 0;
+const p534 = row["pageVPanelTable10Number7"] || 0;
+const p536 = row["pageVPanelTable10Number11"] || 0;
+const p549_1 = row["pageVTableNumber7"] || 0;
+const p564_1 = row["pageVPanelTable11Number7"] || 0;
+const p570 = row["pageVPanelTable12Number31"] || 0;
+const p500_3 = row["pageVPanelTable12Number35"] || 0;
+
+if (pStart) {
+  sum += pStart;
 }
 
-const concatFields = [];
-for (let i = 0; i < data.partniorDataGrid.length; i++) {
-  const partnior = data.partniorDataGrid[i]["partnior"];
-  const pStart = parseInt(data.partniorDataGrid[i]["partniorDataGrid2"]);
-  let iLast = parseInt(data.partniorDataGrid[i]["partniorDataGrid4"]);
-  let iiLast = parseInt(data.partniorDataGrid[i]["partniorDataGrid6"]);
-  let iiiLast = parseInt(data.partniorDataGrid[i]["partniorDataGrid5"]);
-  const pEnd = parseInt(data.partniorDataGrid[i]["partniorDataGrid3"]);
-  const country = data.partniorDataGrid[i]["page37"];
-
-  const isCountryNotOne = isEmpty(country) || country !== 1;
-
-  if (
-    !isEmpty(partnior) &&
-    isCountryNotOne &&
-    (pStart >= 10 || pEnd >= 10 || iLast >= 10 || iiLast >= 10 || iiiLast >= 10)
-  ) {
-    concatFields.push(partnior);
-  }
+if (p520) {
+  sum += p520;
 }
 
-for (let i = 0; i < data.ararezidentPartniorDatagrid.length; i++) {
-  if (!isEmpty(data.ararezidentPartniorDatagrid[i]["ararezidentPartnior"])) {
-    concatFields.push(
-      data.ararezidentPartniorDatagrid[i]["ararezidentPartnior"],
-    );
-  }
+if (p522) {
+  sum -= p522;
 }
 
-values = concatFields;
+if (p530) {
+  sum += p530;
+}
+
+if (p531) {
+  sum -= p531;
+}
+
+if (p532) {
+  sum -= p532;
+}
+
+if (p534) {
+  sum -= p534;
+}
+
+if (p536) {
+  sum -= p536;
+}
+
+if (p549_1) {
+  sum += p549_1;
+}
+
+if (p564_1) {
+  sum += p564_1;
+}
+
+if (p570) {
+  sum += p570;
+}
+
+if (p500_3) {
+  sum += p500_3;
+}
+
+sum = parseFloat(sum.toFixed(4));
+
+const alertNumber = parseFloat((e1S2 - sum).toFixed(4));
+
+if (sum === e1S2) {
+  valid = true;
+} else {
+  valid = "გადაამოწმეთ სესხის თანხები, სხვაობაა " + alertNumber;
+}
